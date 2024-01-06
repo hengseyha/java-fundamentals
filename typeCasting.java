ff758230dbc12bd406aea748d4cb5ac5ded498da
Heng Seyha <heng@mad.co>
2024-01-06 10:34:51 +0700

    typeCasting

---
+++ typeCasting.java
@@ -0,0 +1,11 @@
+public class typeCasting {
+    public static void main(String[] args) {
+        int value = 1200;
+        long longValue = value;
+        double doubleValue = longValue;
+
+        System.out.println(value);
+        System.out.println(longValue);
+        System.out.println(doubleValue);
+    }
+}
